0b42a30e6eb18a28ddafd00cab9b2f87f9b38671
Sali Mallat <92052904+s-mallat@users.noreply.github.com>
2022-02-03 11:10:09 +0000

    Add files via upload

---
+++ Homework_Boids/testApp.h
@@ -0,0 +1,35 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+
+#include "ofMain.h"
+#include <vector>
+#include "boid.h"
+#include "baby_boid.hpp"
+
+class testApp : public ofBaseApp{
+	
+public:
+    ~testApp();
+	
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    
+    int screenW;
+    int screenH;
+
+    std::vector<Boid *> boids;
+    std::vector<Boid *> newBoids; // Declaring 2nd dynamic vector array
+
+};
+
+#endif	
