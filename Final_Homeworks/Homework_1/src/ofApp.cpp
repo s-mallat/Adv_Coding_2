513a36c5af2dd040bb6bbc6939719eb594428e7c
Sali Mallat <92052904+s-mallat@users.noreply.github.com>
2022-03-16 23:46:09 +0000

    Add files via upload

---
+++ Final_Homeworks/Homework_1/src/ofApp.cpp
@@ -0,0 +1,138 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    //ofSetBackgroundAuto(false);
+    ofSetCircleResolution(100);
+    ofSetFrameRate(80);
+    ofSetRectMode(OF_RECTMODE_CENTER);
+    
+    int sampleRate = 44100;
+    int bufferSize= 512;
+    ofxMaxiSettings::setup(sampleRate, 2, bufferSize);
+    
+    // Setup ofSound
+    ofSoundStreamSettings settings;
+    settings.setOutListener(this);
+    settings.sampleRate = sampleRate;
+    settings.numOutputChannels = 2;
+    settings.numInputChannels = 0;
+    settings.bufferSize = bufferSize;
+    soundStream.setup(settings);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    rotate++;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    theta = theta + .03;
+    amplitude = 200;
+    frequency = 0.01;
+    
+    // Waves
+    
+        
+    time = ofGetElapsedTimef();
+    
+    
+    ofNoFill();
+
+    ofSetColor(157, 70);
+    
+    for (int i = 0; i < 5; i++) {
+
+        for (int j = 0; j < 140; j++) {
+            size = 70 * tan(sin(j + time)  + ofGetMouseX()/50);
+            ofSetLineWidth(ofRandom(1,2.5));
+
+            x = ofGetWidth()/2 + size * sin(tan(sin(j))*0.05) * 70 ;
+            y = ofGetHeight()/2;
+
+            ofDrawCircle(x , y , size * i);
+        }
+
+    }
+
+}
+
+void ofApp::audioOut(ofSoundBuffer& output){
+    std::size_t outChannels = output.getNumChannels();
+    freq = 100;
+    
+//    osc1.sinewave(200 + (osc2.sinewave(0.15) * 100.0) *     osc3.sinewave(0.20) * 20) + osc4.sinewave(0.10) * 4 + osc5.sinewave(40) + osc6.sinewave(200);
+    
+ //   myOsc1.sinewave(sin(myOsc2.coswave(freq)*0.05) * 70) * 10;
+    for (int i = 0; i < output.getNumFrames(); ++i){
+
+        output[i * outChannels] = myOsc1.sinewave(200 + (myOsc2.coswave(0.15) * 100) * myOsc3.coswave(0.20) * 20 + myOsc4.sinewave(0.10)) * myOsc5.sinewave(40) + myOsc6.sinewave(200);
+        output[i * outChannels + 1] = output[i * outChannels];
+        
+        //Hold the values so the draw method can draw them
+//        waveform[waveIndex] =  output[i * outChannels];
+//        if (waveIndex < (ofGetWidth() - 1)) {
+//            ++waveIndex;
+//        } else {
+//            waveIndex = 0;
+//        }
+    }
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
